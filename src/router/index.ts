d4e9729eecfc6f926d1b9bbcaff67a918fafb7ad
yuichiyasui <yassan.b.a.w@gmail.com>
2021-07-18 10:27:32 +0900

    ルームページのモックを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Top from "@/components/top/index.vue";
 import Rooms from "@/components/rooms/index.vue";
+import Room from "@/components/room/index.vue";
 
 const history = createWebHistory();
 
 const routes = [
   { path: "/rooms", name: "rooms", component: Rooms },
+  { path: "/room/:roomId", name: "room", component: Room },
   { path: "/", name: "top", component: Top },
 ];
 
