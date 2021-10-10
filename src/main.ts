51a89f641ffc07f1b20586c25dcaf13954d16817
yuichiyasui <yassan.b.a.w@gmail.com>
2021-10-11 02:01:19 +0900

    tailwind cssを導入

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import router from "@/router/index";
 import { store, key } from "@/store/index";
 
+import "./assets/css/index.css";
+
 import App from "@/App.vue";
 
 const app = createApp(App);
