63ed249e2431ecfe47ab34f472c6465e12ce0bc3
yuichiyasui <yassan.b.a.w@gmail.com>
2021-10-10 23:47:36 +0900

    共通ヘッダーにユーザー情報を表示

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
-	<router-view />
+  <header>
+    <p class="user">
+      {{ userName + "さんがログイン中" }}
+    </p>
+  </header>
+  <router-view />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+import { useStore } from "vuex";
+import { key } from "./store";
 
 export default defineComponent({
-	name: "App",
+  name: "App",
+  setup() {
+    const store = useStore(key);
+
+    return {
+      userName: computed(() => store.state.user.user?.name || "ゲスト"),
+    };
+  },
 });
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
