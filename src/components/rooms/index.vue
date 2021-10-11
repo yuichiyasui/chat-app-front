9c5be0753c5d1b45ff2bd1659b5786b4d8b9a17a
yuichiyasui <yassan.b.a.w@gmail.com>
2021-10-11 23:46:31 +0900

    ルーム一覧のデザインを作成

--- src/components/rooms/index.vue
+++ src/components/rooms/index.vue
@@ -1,42 +1,97 @@
 <template>
-  <h1>チャットルーム一覧</h1>
-  <button @click="openCreateRoomModal">ルームを作成</button>
+  <main>
+    <div class="bg-white mx-auto mt-20 py-16 px-20 w-max rounded-lg shadow-sm">
+      <h1 class="text-2xl font-bold text-center mb-5">チャットルーム一覧</h1>
 
-  <div
-    v-if="state.isShowCreateRoomModal"
-    @click.self="closeCreateRoomModal"
-    class="overlay"
-  >
-    <section class="create-room-modal">
-      <h2 class="create-room-modal_title">ルームを作成する</h2>
-      <form @submit.prevent="createRoom" class="create-room-modal_form">
-        <label class="create-room-modal_form_label">
-          <p class="create-room-modal_form_name">ルーム名</p>
-          <div class="create-room-modal_form_container">
-            <input
-              type="text"
-              name="name"
-              v-model="state.form.name"
-              required
-              class="create-room-modal_form_input"
-            />
-          </div>
-        </label>
-        <button type="submit" class="create-room-modal_form_submit-button">
-          作成
-        </button>
-      </form>
-    </section>
-  </div>
-
-  <ol>
-    <li v-for="room in state.rooms" :key="room.id">
-      <router-link :to="{ name: 'room', params: { roomId: room.id } }">
-        {{ room.name }}
+      <ol class="mb-10">
+        <template v-if="state.isFetchingRooms">
+          <li
+            v-for="i in 2"
+            :key="`fake-room-${i}`"
+            class="animate-pulse mb-4 last:mb-0"
+          >
+            <div class="bg-gray-200 h-7 rounded-sm"></div>
+          </li>
+        </template>
+        <template v-else-if="state.rooms.length">
+          <li
+            v-for="room in state.rooms"
+            :key="room.id"
+            class="text-center h-7 mb-4 last:mb-0"
+          >
+            <router-link
+              :to="{ name: 'room', params: { roomId: room.id } }"
+              class="text-blue-500 hover:text-blue-700 text-lg"
+            >
+              {{ room.name }}
+            </router-link>
+          </li>
+        </template>
+      </ol>
+      <button
+        class="
+          table
+          mx-auto
+          bg-blue-500
+          text-white
+          font-bold
+          rounded-md
+          px-8
+          py-4
+          hover:bg-opacity-80
+          shadow-sm
+          mb-5
+        "
+        @click="openCreateRoomModal"
+      >
+        ルームを作成
+      </button>
+      <router-link
+        :to="{ name: 'top' }"
+        class="
+          table
+          mx-auto
+          bg-green-500
+          text-white
+          font-bold
+          rounded-md
+          px-8
+          py-4
+          hover:bg-opacity-80
+          shadow-sm
+        "
+      >
+        トップに戻る
       </router-link>
-    </li>
-  </ol>
-  <router-link :to="{ name: 'top' }">トップ</router-link>
+    </div>
+
+    <div
+      v-if="state.isShowCreateRoomModal"
+      @click.self="closeCreateRoomModal"
+      class="overlay"
+    >
+      <section class="create-room-modal">
+        <h2 class="create-room-modal_title">ルームを作成する</h2>
+        <form @submit.prevent="createRoom" class="create-room-modal_form">
+          <label class="create-room-modal_form_label">
+            <p class="create-room-modal_form_name">ルーム名</p>
+            <div class="create-room-modal_form_container">
+              <input
+                type="text"
+                name="name"
+                v-model="state.form.name"
+                required
+                class="create-room-modal_form_input"
+              />
+            </div>
+          </label>
+          <button type="submit" class="create-room-modal_form_submit-button">
+            作成
+          </button>
+        </form>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -48,6 +103,7 @@ type State = {
   rooms: Room[];
   form: { name: string };
   isShowCreateRoomModal: boolean;
+  isFetchingRooms: boolean;
 };
 
 export default defineComponent({
@@ -57,6 +113,7 @@ export default defineComponent({
       rooms: [],
       form: { name: "" },
       isShowCreateRoomModal: false,
+      isFetchingRooms: true,
     });
 
     const openCreateRoomModal = () => {
@@ -80,6 +137,9 @@ export default defineComponent({
 
     onBeforeMount(async () => {
       state.rooms = await api.showRooms();
+      setTimeout(() => {
+        state.isFetchingRooms = false;
+      }, 1000);
     });
 
     return { state, openCreateRoomModal, closeCreateRoomModal, createRoom };
