8445a61d26c16dad905ea6bc466886b6ea60d161
yuichiyasui <yassan.b.a.w@gmail.com>
2021-11-01 02:00:25 +0900

    apiのurlを環境変数に定義

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,7 +2,7 @@ async function wrappedFetch(
   path: string,
   option: RequestInit
 ): Promise<Response> {
-  const ORIGIN = "http://localhost:3500";
+  const ORIGIN = import.meta.env.VITE_API_ORIGIN;
   const CONTENT_TYPE = "application/json;charset=UTF-8";
   const response = await fetch(`${ORIGIN}${path}`, {
     headers: {
